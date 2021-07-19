323c79df8e34135927c8497d300f08c849cc9bea
Nesomar Ramalho Freire <nfreire@latam.stefanini.com>
2021-07-19 16:37:28 -0300

    Estrutura inicial do projeto

---
+++ crud/src/main/java/br/com/academy/crud/entity/Produto.java
@@ -0,0 +1,37 @@
+package br.com.academy.crud.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 3798471342993606121L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false, length = 255)
+	private String nome;
+	
+	@Column(name = "nome", nullable = false, length = 10)
+	private Integer estoque;
+	
+	@Column(name = "nome", nullable = false, length = 10)
+	private Double preco;
+}
---
+++ crud/src/main/java/br/com/academy/model/MesangemErrorVO.java
@@ -0,0 +1,9 @@
+package br.com.academy.model;
+
+import java.io.Serializable;
+
+public class MesangemErrorVO implements Serializable {
+
+	private static final long serialVersionUID = 1368277746709962506L;
+
+}
